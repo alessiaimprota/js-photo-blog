fa731a98b03b3193a774018a33b8327e9e2e248e
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-09 11:26:01 +0100

    close and open of the static overlay

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-
 //salvo l'endpoint in una costante e anche l'output che avremo in pagina
-const endpoint= "https://lanciweb.github.io/demo/api/pictures/"
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 //console.log(endpoint)
-const output= document.getElementById("container")
+const output = document.getElementById("container");
 
 //aggiungo altre variabili per gestire l'apertura e la chiusura dell'overlay
-const overlay= document.getElementById("overlay")
-const btnOverlay=document.getElementById("btn-overlay")
+const overlay = document.getElementById("overlay");
+const btnOverlay = document.getElementById("btn-overlay");
 
 //faccio la chiamata all'endpoint
-axios.get(endpoint)
-    .then(response => {
-        //console.log(response.data)
-        //ottenuto l'array di obj lo salvo in una variabile
-        const photos= response.data
-        //console.log(photos)
+axios
+  .get(endpoint)
+  .then((response) => {
+    //console.log(response.data)
+    //ottenuto l'array di obj lo salvo in una variabile
+    const photos = response.data;
+    //console.log(photos)
 
-        //salviamo una variabile vuota di accumulo per l'output
-        let photosOutput = ""
+    //salviamo una variabile vuota di accumulo per l'output
+    let photosOutput = "";
 
-        //cicliamo l'array
-        photos.forEach(photo => {
-            
-        //destrutturiamo e salviamo i singoli item degli obj
-        
-            const { title, date, url} = photo;
+    //cicliamo l'array
+    photos.forEach((photo) => {
+      //destrutturiamo e salviamo i singoli item degli obj
 
-        
-        photosOutput += `
+      const { title, date, url } = photo;
+
+      photosOutput += `
         <div class="col-lg-4 col-md-6 col-12  g-5 p-3">
-         <div class="card bg-light position-relative border-0">
+        <button class="btn-card">
+        <div class="card bg-light position-relative border-0">
           <img src="img/pin.svg" alt="pin" id="pin" class="position-absolute top-0 start-50 translate-middle">
           <img src="${url}" alt="travel-photo" />
           <div class="photo-description pt-1">
@@ -37,17 +36,26 @@ axios.get(endpoint)
           <p>${date}</p>
           </div>
           </div>
-        </div>
-        `
-        });
-        
-        output.innerHTML=photosOutput;
-    })
-      .catch(error => {
-        console.log(error);
-        output.innerHTML = "<p>Impossibile caricare le foto al momento.</p>";});
+          </button>
+          </div>
+          `;
+    });
 
+    output.innerHTML = photosOutput;
+    const btnCards = document.querySelectorAll(".btn-card");
 
-        btnOverlay.addEventListener("click", ()=>{
-            overlay.style.display="none"
-        })
+    btnCards.forEach((btnCard) => {
+      btnCard.addEventListener("click", () => {
+        overlay.classList.remove("d-none");
+        overlay.classList.add("d-block");
+      });
+    });
+    btnOverlay.addEventListener("click", () => {
+      overlay.classList.remove("d-block");
+      overlay.classList.add("d-none");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    output.innerHTML = "<p>Impossibile caricare le foto al momento.</p>";
+  });
